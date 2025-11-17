9e128ca072cc0c3a102a99e3ef24ac8b132e89a5
Tomas-Castano <tomas.castanoo@uqvirtual.edu.co>
2025-11-16 23:27:29 -0500

    Reestructuración adapters + persistence/ets_repo.ex

--- adapters/persistence/ets_repo.ex
+++ adapters/persistence/ets_repo.ex
@@ -1,171 +1,139 @@
+# Adapters/Persistence/ets_repo.ex
 defmodule Adapters.Persistence.ETSRepo do
-  @behaviour Adapters.Persistence.RepoBehavior
+  @moduledoc """
+  Repositorio de persistencia usando ETS (Erlang Term Storage)
+  Almacena datos en memoria durante la ejecución
+  """
 
-  @participants_table :participants_table
-  @teams_table        :teams_table
-  @projects_table     :projects_table
-  @mentors_table      :mentors_table
+  use GenServer
 
-  @messages_table       :messages_table       # mensajes por equipo
-  @global_messages_table :global_messages_table
+  # Nombres de las tablas ETS
+  @participantes :participantes_table
+  @equipos :equipos_table
+  @proyectos :proyectos_table
+  @mentores :mentores_table
+  @mensajes :mensajes_table
+  @salas_tematicas :salas_tematicas_table
 
-  # ============================================================
-  #  INIT: se llama al arrancar el CLI o al supervisor
-  # ============================================================
-  def start_link() do
-    create_table(@participants_table)
-    create_table(@teams_table)
-    create_table(@projects_table)
-    create_table(@mentors_table)
+  # ============================================================================
+  # API Pública (Cliente)
+  # ============================================================================
 
-    create_table(@messages_table)
-    create_table(@global_messages_table)
-
-    {:ok, self()}
+  def start_link(_opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  defp create_table(name) do
-    :ets.new(name, [:named_table, :set, :public, read_concurrency: true])
+  # --- Participantes ---
+  def guardar_participante(participante), do: GenServer.call(__MODULE__, {:guardar, @participantes, participante.id, participante})
+  def obtener_participante(id), do: GenServer.call(__MODULE__, {:obtener, @participantes, id})
+  def listar_participantes(), do: GenServer.call(__MODULE__, {:listar, @participantes})
+
+  # --- Equipos ---
+  def guardar_equipo(equipo), do: GenServer.call(__MODULE__, {:guardar, @equipos, equipo.id, equipo})
+  def obtener_equipo(id), do: GenServer.call(__MODULE__, {:obtener, @equipos, id})
+  def listar_equipos(), do: GenServer.call(__MODULE__, {:listar, @equipos})
+  def buscar_equipo_por_nombre(nombre), do: GenServer.call(__MODULE__, {:buscar_por_nombre, nombre})
+
+  # --- Proyectos ---
+  def guardar_proyecto(proyecto), do: GenServer.call(__MODULE__, {:guardar, @proyectos, proyecto.id, proyecto})
+  def obtener_proyecto(id), do: GenServer.call(__MODULE__, {:obtener, @proyectos, id})
+  def listar_proyectos(), do: GenServer.call(__MODULE__, {:listar, @proyectos})
+  def obtener_proyecto_por_equipo(equipo_id), do: GenServer.call(__MODULE__, {:proyecto_por_equipo, equipo_id})
+
+  # --- Mentores ---
+  def guardar_mentor(mentor), do: GenServer.call(__MODULE__, {:guardar, @mentores, mentor.id, mentor})
+  def obtener_mentor(id), do: GenServer.call(__MODULE__, {:obtener, @mentores, id})
+  def listar_mentores(), do: GenServer.call(__MODULE__, {:listar, @mentores})
+
+  # --- Mensajes ---
+  def guardar_mensaje(mensaje), do: GenServer.call(__MODULE__, {:guardar_mensaje, mensaje})
+  def listar_mensajes(tipo, destino_id, limite \\ 50), do: GenServer.call(__MODULE__, {:listar_mensajes, tipo, destino_id, limite})
+
+  # --- Salas Temáticas ---
+  def guardar_sala(sala), do: GenServer.call(__MODULE__, {:guardar, @salas_tematicas, sala.id, sala})
+  def obtener_sala(id), do: GenServer.call(__MODULE__, {:obtener, @salas_tematicas, id})
+  def listar_salas(), do: GenServer.call(__MODULE__, {:listar, @salas_tematicas})
+
+  # ============================================================================
+  # Callbacks de GenServer
+  # ============================================================================
+
+  @impl true
+  def init(:ok) do
+    # Crear todas las tablas ETS
+    crear_tabla(@participantes)
+    crear_tabla(@equipos)
+    crear_tabla(@proyectos)
+    crear_tabla(@mentores)
+    crear_tabla(@mensajes)
+    crear_tabla(@salas_tematicas)
+
+    {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call({:guardar, tabla, id, dato}, _from, state) do
+    :ets.insert(tabla, {id, dato})
+    {:reply, :ok, state}
+  end
+
+  @impl true
+  def handle_call({:obtener, tabla, id}, _from, state) do
+    resultado = case :ets.lookup(tabla, id) do
+      [{^id, dato}] -> dato
+      [] -> nil
+    end
+    {:reply, resultado, state}
+  end
+
+  @impl true
+  def handle_call({:listar, tabla}, _from, state) do
+    lista = :ets.tab2list(tabla)
+            |> Enum.map(fn {_id, dato} -> dato end)
+    {:reply, lista, state}
+  end
+
+  @impl true
+  def handle_call({:buscar_por_nombre, nombre}, _from, state) do
+    resultado = :ets.tab2list(@equipos)
+                |> Enum.map(fn {_id, equipo} -> equipo end)
+                |> Enum.find(&(&1.nombre == nombre))
+    {:reply, resultado, state}
+  end
+
+  @impl true
+  def handle_call({:proyecto_por_equipo, equipo_id}, _from, state) do
+    resultado = :ets.tab2list(@proyectos)
+                |> Enum.map(fn {_id, proyecto} -> proyecto end)
+                |> Enum.find(&(&1.equipo_id == equipo_id))
+    {:reply, resultado, state}
+  end
+
+  @impl true
+  def handle_call({:guardar_mensaje, mensaje}, _from, state) do
+    # Clave compuesta: {tipo, destino_id, timestamp}
+    clave = {mensaje.tipo, mensaje.destino_id, System.system_time(:millisecond)}
+    :ets.insert(@mensajes, {clave, mensaje})
+    {:reply, :ok, state}
+  end
+
+  @impl true
+  def handle_call({:listar_mensajes, tipo, destino_id, limite}, _from, state) do
+    mensajes = :ets.tab2list(@mensajes)
+               |> Enum.filter(fn {{t, d, _ts}, _msg} -> t == tipo and d == destino_id end)
+               |> Enum.sort_by(fn {{_t, _d, ts}, _msg} -> ts end, :desc)
+               |> Enum.take(limite)
+               |> Enum.map(fn {_key, msg} -> msg end)
+    {:reply, mensajes, state}
+  end
+
+  # ============================================================================
+  # Funciones privadas
+  # ============================================================================
+
+  defp crear_tabla(nombre) do
+    :ets.new(nombre, [:named_table, :set, :public, read_concurrency: true])
   rescue
-    _ -> :ok     # si ya existe la tabla, no explota
-  end
-
-  # ============================================================
-  # PARTICIPANTS (PARTICIPANTES)
-  # ============================================================
-  def save_participant(participant) do
-    :ets.insert(@participants_table, {participant.id, participant})
-    :ok
-  end
-
-  def get_participant(id) do
-    case :ets.lookup(@participants_table, id) do
-      [{^id, participant}] -> participant
-      _ -> nil
-    end
-  end
-
-  def list_participants() do
-    :ets.tab2list(@participants_table)
-    |> Enum.map(fn {_id, participant} -> participant end)
-  end
-
-  # ============================================================
-  # TEAMS (EQUIPOS)
-  # ============================================================
-  def save_team(team) do
-    :ets.insert(@teams_table, {team.id, team})
-    :ok
-  end
-
-  def get_team(id) do
-    case :ets.lookup(@teams_table, id) do
-      [{^id, team}] -> team
-      _ -> nil
-    end
-  end
-
-  def list_teams() do
-    :ets.tab2list(@teams_table)
-    |> Enum.map(fn {_id, team} -> team end)
-  end
-
-  # ============================================================
-  # PROJECTS (PROYECTOS)
-  # ============================================================
-  def save_project(project) do
-    :ets.insert(@projects_table, {project.id, project})
-    :ok
-  end
-
-  def get_project(id) do
-    case :ets.lookup(@projects_table, id) do
-      [{^id, project}] -> project
-      _ -> nil
-    end
-  end
-
-  def list_projects() do
-    :ets.tab2list(@projects_table)
-    |> Enum.map(fn {_id, p} -> p end)
-  end
-
-  def get_project_by_team(team_id) do
-    :ets.tab2list(@projects_table)
-    |> Enum.map(fn {_id, p} -> p end)
-    |> Enum.find(fn p -> p.team_id == team_id end)
-  end
-
-  # ============================================================
-  # MENTORES
-  # ============================================================
-  def save_mentor(mentor) do
-    :ets.insert(@mentors_table, {mentor.id, mentor})
-    :ok
-  end
-
-  def get_mentor(id) do
-    case :ets.lookup(@mentors_table, id) do
-      [{^id, mentor}] -> mentor
-      _ -> nil
-    end
-  end
-
-  def list_mentors() do
-    :ets.tab2list(@mentors_table)
-    |> Enum.map(fn {_id, m} -> m end)
-  end
-
-  # ============================================================
-  # MENSAJES POR EQUIPO
-  # messages_table = {team_id, [msg1, msg2, msg3]}
-  # ============================================================
-  def save_message(team_id, msg) do
-    msgs =
-      case :ets.lookup(@messages_table, team_id) do
-        [{^team_id, list}] -> [msg | list]
-        _ -> [msg]
-      end
-
-    :ets.insert(@messages_table, {team_id, msgs})
-    :ok
-  end
-
-  def list_messages(team_id) do
-    case :ets.lookup(@messages_table, team_id) do
-      [{^team_id, msgs}] -> Enum.reverse(msgs)
-      _ -> []
-    end
-  end
-
-  # MENSAJES GLOBALES
-  def save_global_message(msg) do
-    list =
-      case :ets.lookup(@global_messages_table, :global) do
-        [global: msgs] -> [msg | msgs]
-        _ -> [msg]
-      end
-
-    :ets.insert(@global_messages_table, {:global, list})
-    :ok
-  end
-
-  def list_global_messages() do
-    case :ets.lookup(@global_messages_table, :global) do
-      [global: msgs] -> Enum.reverse(msgs)
-      _ -> []
-    end
-  end
-
-  # ============================================================
-  # ANUNCIOS (Save announcements as global messages)
-  # ============================================================
-  def save_announcement(announcement) do
-    save_global_message(announcement)
-  end
-
-  def list_announcements(limit) do
-    list_global_messages()
-    |> Enum.take(limit)
+    ArgumentError -> :ok  # La tabla ya existe
   end
 end
